7b0b31dacb8e9fbf31213d9674bee5ea548a4e20
Spencer Rohan <spencermrohan@gmail.com>
2017-02-02 18:29:34 -0600

    Skeleton bedrock setup

--- index.php
+++ index.php
@@ -1,30 +1,5 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Troubadour Hostel</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="Troubadour Hostel">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" type="text/css" href="assets/css/app.css">	
-	</head>
-	
-	<body>
-		<!-- Header -->
-		<header>
-		</header>
-	
-		
-		<!-- Main -->
-		<main>
-		</main>
+<?php
 
-		
-		<!-- Scripts -->
-		<script type="text/javascript" src="assets/js/app.js"></script>
-
-		<!-- Footer -->
-		<footer>
-		</footer>
-
-	</body>
-</html>
+/** WordPress view bootstrapper */
+define('WP_USE_THEMES', true);
+require(__DIR__ . '/wp/wp-blog-header.php');
