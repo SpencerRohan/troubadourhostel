a070015c894bc69c512fff73f3f5231439354c3c
Spencer Rohan <spencermrohan@gmail.com>
2017-02-06 00:08:29 -0600

    ADD primary menu structure

--- app/themes/troubadourhostel/header.php
+++ app/themes/troubadourhostel/header.php
@@ -33,14 +33,16 @@
 	</head>
 	<body <?php body_class(); ?>>
 				<!-- header -->
-				<header class="header clear">
+				<header class="header clear <?php echo is_home() ? 'parallax-container' : ''; ?>">
 						<!-- nav -->
 						<nav class="nav" role="navigation">
-							
+							<?php wp_nav_menu( array( 'troubadourhostel' => 'primary' ) ); ?>
 						</nav>
 						<!-- /nav -->
 
 						<?php if( is_home() ): ?>
+							<div class="parallax"><img src="http://d2436y6oj07al2.cloudfront.net/assets/vbblog/2015/02/River.jpg"></div>
+
 							<div class="brand-title">
 								<div class="title">
 									<h1 class="brand-troubadour">troubadour</h1>
@@ -54,7 +56,5 @@
 								</div>
 							</div>
 						<?php endif; ?>
-						
-
 				</header>
 				<!-- /header -->
--- app/themes/troubadourhostel/src/js/app.js
+++ app/themes/troubadourhostel/src/js/app.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+		$('.parallax').parallax();
     console.log("HERE");
 
 });
